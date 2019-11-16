a46d3480aaf97cba695eb19c9661c4f18662bedc
Manutai Teraiamano <manutai@teraiamano.com>
2019-11-16 19:39:38 +0100

    Revert "Merge branch 'BougezvotreQ' of https://github.com/Alpagatosaur/thg into BougezvotreQ"
    This reverts commit 6ded3fd807f4fb77991861a25064da74742a83b9, reversing
    changes made to f19b386a3293ccf19a5fd0e5bbea44db438c6487.

--- src/projet/Test.java
+++ src/projet/Test.java
@@ -4,52 +4,53 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
 
-public class Test
+public class Test 
 {
 
-	public static void main(String[] args) throws IOException {
-		try {
-
-			System.out.println("Quel graphe voulez vous ï¿½tudier? ( -1 == NON )");
-			Scanner action = new Scanner(System.in);
-			int nb = action.nextInt();
-
-			while (nb != -1) {
-				
-				String M = new File("").getAbsolutePath()+"/src/projet/Graphe"+nb+ ".txt"; // On cherche un graphe.txt
-
-				System.out.println("Lecture du graphe sur fichier");
-				Methode.Lire(M);
+	public static void main(String[] args) throws IOException 
+	{
+		try
+		{
+		System.out.println("Quel graphe voulez vous étudier? ( -1 == NON )");
+		Scanner action = new Scanner(System.in);
+		int nb = action.nextInt();
+		
+		while (nb != -1)
+		{
+			String M = new File("").getAbsolutePath()+"/src/projet/Graphe"+nb+ ".txt"; // On cherche un graphe.txt
 			
-				System.out.println("");
-				System.out.println("Reprï¿½sentation du graphe sous forme matricielle");
-				Methode.adjacence(M);
-				Methode.valeurs(M);
-
-				System.out.println("");
-				System.out.println("Dï¿½tection de circuit");
-				Methode.circuit(M);
-
-
-				System.out.println("");
-				System.out.println("Calcul des rangs");
-				Methode.rang(M);
-
-
-
-				System.out.println("Quel graphe voulez vous afficher?");
-				Scanner number = new Scanner(System.in);
-				nb = number.nextInt(); // Si nb == -1 : ArrÃªt de la boucle
-
-			}
-
-			System.out.println("Fin");
-			action.close();
-
-		} catch (Exception e) {
+			System.out.println("Lecture du graphe sur fichier");
+			Methode.Lire(M);
 			
-			System.err.println(e + "Un problï¿½me est survenu");
+			System.out.println("");
+			System.out.println("Représentation du graphe sous forme matricielle");
+			Methode.adjacence(M);
+			Methode.valeurs(M);
 			
+			System.out.println("");
+			System.out.println("Détection de circuit");
+			Methode.circuit(M);
+			
+			
+			System.out.println("");
+			System.out.println("Calcul des rangs");
+			Methode.rang(M);
+			
+
+			
+			System.out.println("Quel graphe voulez vous afficher?");
+			Scanner number = new Scanner(System.in);
+			nb = number.nextInt(); // Si nb == -1 On arrête la boucle
+			
+		}
+		
+		System.out.println("Fin");
+		action.close();
+		}
+		
+		catch (Exception e)
+		{
+		System.err.println(e + "Un problème est survenu");
 		}
 	}
 
